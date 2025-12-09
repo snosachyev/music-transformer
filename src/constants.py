9ff001e765bf5513e4f6a296e4b585e029d50908
Сергей Носачёв <crackkc@mail.ru>
2025-12-09 18:56:46 +0300

    fix contants

--- src/constants.py
+++ src/constants.py
@@ -30,7 +30,7 @@ print("Device:", DEVICE)
 # ---------------------------------------------------------
 # Basic config
 # ---------------------------------------------------------
-ROOT = Path(__file__).resolve().parent
+ROOT = Path(__file__).resolve().parent.parent
 SRC = ROOT / "src"
 if str(SRC) not in sys.path:
     sys.path.insert(0, str(SRC))
